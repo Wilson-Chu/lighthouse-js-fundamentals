a9264685db02dc85436cc4764a1019aa5e1b4c1b
Wilson Chu <wilson1.chu@gmail.com>
2023-07-31 01:56:28 -0400

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,3 +1,18 @@
+/* EXERCISE 1 */
+const raining = true;
+const cold = false;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+}
+
+console.log("Now you're ready to go outside!");
+
+/* EXERCISE 2 */
 const temperature = 15;
 
 if (temperature < 0) {
@@ -9,3 +24,19 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+
+/* EXERCISE 3 */
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
