ae177c136aba7f52f36fe8a189e2aca9bf0c86da
Wilson Chu <wilson1.chu@gmail.com>
2023-08-11 02:25:30 -0400

    more LHL practice with arrays and loops

---
+++ lastIndexOf.js
@@ -0,0 +1,16 @@
+// Finding the last index where target exists in array, without using .indexOf function
+const lastIndexOf = function (array, target) {
+  for (let i = array.length - 1; i >= 0; i--) {
+    if (array[i] === target) {
+      return i;
+    }
+  }
+
+  return -1;
+}
+
+console.log(lastIndexOf([0, 1, 4, 1, 2], 1), "=?", 3);
+console.log(lastIndexOf([0, 1, 4, 1, 2], 2), "=?", 4);
+console.log(lastIndexOf([0, 1, 4, 1, 2], 3), "=?", -1);
+console.log(lastIndexOf([5, 5, 5], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
