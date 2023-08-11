edc893f329cd15a9cfe9cca1a9708d40cb0a9767
Wilson Chu <wilson1.chu@gmail.com>
2023-08-11 02:34:51 -0400

    Modified loopyLighthouse.js for LHL Stretch exercise

--- loopyLighthouse.js
+++ loopyLighthouse.js
@@ -1,17 +1,21 @@
-const loopy = "Loopy";
-const lighthouse = "Lighthouse";
+const loopyLighthouse = function (range, multiples, words) {
+  const loopy = words[0];
+  const lighthouse = words[1];
 
-for (let i = 100; i <= 200; i++) {
-  if (i % 12 === 0) {
-    console.log(loopy + lighthouse);
-  }
-  else if (i % 3 === 0) {
-    console.log(loopy);
-  }
-  else if (i % 4 === 0) {
-    console.log(lighthouse);
-  }
-  else {
-    console.log(i);
+  for (let i = range[0]; i <= range[1]; i++) {
+    if (i % (multiples[0] * multiples[1]) === 0) {
+      console.log(loopy + lighthouse);
+    }
+    else if (i % multiples[0] === 0) {
+      console.log(loopy);
+    }
+    else if (i % multiples[1] === 0) {
+      console.log(lighthouse);
+    }
+    else {
+      console.log(i);
+    }
   }
 }
+
+loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
